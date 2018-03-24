b03ce019be5e463ea7a8e303b55803444e58b1d6
Jurn Overmars <jovermars@gwsn.nl>
2018-03-24 14:57:24 +0100

    Initial commit

---
+++ src/Models/AccessControll.php
@@ -0,0 +1,10 @@
+<?php
+namespace Gwsn\Authentication\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccessControll extends Model
+{
+
+}
---
+++ src/Models/User.php
@@ -0,0 +1,11 @@
+<?php
+namespace Gwsn\Authentication\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+
+
+
+}
